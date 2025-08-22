f16aa954fe2bdd67991b07f20dd6c078bca9884d
Daniel Jr <lopezdanieljr10@gmail.com>
2025-08-22 10:20:06 -0500

    Se inicializo el proyecto flutter

---
+++ android/app/src/main/kotlin/com/example/soccerlife/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.soccerlife
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'pantallas/iniciosesion.dart'; // Importamos tu pantalla de login
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Soccer Life',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+      ),
+      home: const InicioSesionPage(), // Aquí le decimos que muestre tu pantalla de login
+    );
+  }
+}
